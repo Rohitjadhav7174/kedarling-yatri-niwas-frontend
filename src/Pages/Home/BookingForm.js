2d3f37efd01852ef2363111b090a896d81f530f4
Rohit Jadhav <113785547+Rohitjadhav7174@users.noreply.github.com>
2025-06-12 00:18:29 +0530

    Update BookingForm.js

--- src/Pages/Home/BookingForm.js
+++ src/Pages/Home/BookingForm.js
@@ -108,7 +108,7 @@ const BookingForm = () => {
   const fetchAvailableRooms = async () => {
     setLoading(true);
     try {
-      const response = await axios.get(`http://kedarling-yatri-niwas-backend.vercel.app/api/rooms/available/${roomType}`);
+      const response = await axios.get(`http://kedarling-yatri-niwas-backend-kci5gkpi8.vercel.app/api/rooms/available/${roomType}`);
       setAvailableRooms(response.data);
     } catch (error) {
       console.error('Error fetching available rooms:', error);
@@ -178,7 +178,7 @@ const handleSubmitBooking = async () => {
       formData.append('paymentProof', paymentProof);
     }
     
-    const response = await axios.post('http://localhost:5001/api/bookings', formData, {
+    const response = await axios.post('http://kedarling-yatri-niwas-backend-kci5gkpi8.vercel.app/api/bookings', formData, {
       headers: {
         'Content-Type': 'multipart/form-data',
       },
@@ -208,7 +208,7 @@ const handleSubmitBooking = async () => {
   const handleCompletePayment = async () => {
     setLoading(true);
     try {
-      await axios.put(`http:/localhost:5001/api/bookings/${bookingId}/payment`, {
+      await axios.put(`http://kedarling-yatri-niwas-backend-kci5gkpi8.vercel.app/api/bookings/${bookingId}/payment`, {
         paymentStatus: 'paid',
         paymentConfirmed: true
       });
